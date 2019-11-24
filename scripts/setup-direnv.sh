1c88c03ce5cf107fbde43ec8842e0016c2203445
zeroneipi <zeroneipi@gmail.com>
2019-11-24 00:41:10 -0700

    add few pkgs and configs

--- scripts/setup-direnv.sh
+++ scripts/setup-direnv.sh
@@ -4,13 +4,13 @@ sudo apt install -y direnv
 
 echo 'Setting direnv to active virtualenv when cd to project dir...'
 
-cat << EOF > $HOME/.bashrc
-eval "$(direnv hook bash)"
+cat << EOF >> $HOME/.bashrc
+eval "\$(direnv hook bash)"
 show_virtual_env() {
-  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename $VIRTUAL_ENV))"
+  if [[ -n "\$VIRTUAL_ENV" && -n "\$DIRENV_DIR" ]]; then
+    echo "(\$(basename \$VIRTUAL_ENV))"
   fi
 }
 export -f show_virtual_env
-PS1='$(show_virtual_env)'$PS1
+PS1='\$(show_virtual_env)'\$PS1
 EOF
--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -7,4 +7,4 @@ ln -sfn /usr/bin/python3.8 /usr/bin/python
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 rm get-pip.py
-pip install virtualenv yapf pylint
+pip install virtualenv yapf pylint ipdb
